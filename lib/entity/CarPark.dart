b5db833e5b27b82a21d02e57e86b3256b5def678
Sdidt <siddharthms607@gmail.com>
2021-02-24 16:34:10 +0800

    Initial Commit

--- lib/entity/CarPark.dart
+++ lib/entity/CarPark.dart
@@ -7,4 +7,25 @@ class CarPark {
   String _carParkType;
   String _parkingType;
   String _freeParking;
+
+  CarPark({
+    String carParkNum,
+    String address,
+    GeoPoint location,
+    String carParkType,
+    String parkingType,
+    String freeParking,
+  })  : _carParkNum = carParkNum,
+        _address = address,
+        _location = location,
+        _carParkType = carParkType,
+        _parkingType = parkingType,
+        _freeParking = freeParking;
+
+  String get carParkNum => _carParkNum;
+  String get address => _address;
+  GeoPoint get location => _location;
+  String get carParkType => _carParkType;
+  String get parkingType => _parkingType;
+  String get freeParking => _freeParking;
 }
