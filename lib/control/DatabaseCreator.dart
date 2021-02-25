d2c6c5c86ec0604208e60828798a1eb013a5857f
Sdidt <siddharthms607@gmail.com>
2021-02-25 12:55:29 +0800

    Initial Commit

--- lib/control/DatabaseCreator.dart
+++ lib/control/DatabaseCreator.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/services.dart';
+import 'package:geojson/geojson.dart';
+
 class DatabaseCreator {
-  createDatabaseForEWaste() {
-    ;
+  static createDatabaseForEWaste() async {
+    String rawGeoJson = await rootBundle
+        .loadString('assets/databases/e-waste-recycling-geojson.geojson');
+    final features = await featuresFromGeoJson(rawGeoJson);
+    print(features);
   }
 }
---
+++ lib/screens/BasicTestingScreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:wastetastic/control/DatabaseCreator.dart';
+
+class BasicTestingScreen extends StatefulWidget {
+  static const String id = 'basic_testing_screen';
+  @override
+  _BasicTestingScreenState createState() => _BasicTestingScreenState();
+}
+
+class _BasicTestingScreenState extends State<BasicTestingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FloatingActionButton(
+        onPressed: () {
+          DatabaseCreator.createDatabaseForEWaste();
+        },
+      ),
+    );
+  }
+}
