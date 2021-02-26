7804da5ac3062aef8271cb3c473684f8a729648c
Sdidt <siddharthms607@gmail.com>
2021-02-26 17:28:40 +0800

    Initial Commit

--- lib/screens/AddWasteScreen.dart
+++ lib/screens/AddWasteScreen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class AddWasteScreen extends StatelessWidget {
+class AddWasteScreen extends StatefulWidget {
+  @override
+  _AddWasteScreenState createState() => _AddWasteScreenState();
+}
+
+class _AddWasteScreenState extends State<AddWasteScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
