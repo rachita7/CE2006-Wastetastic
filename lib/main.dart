6a7ef745584236f54bfad91e9356806b25f88d34
Sdidt <siddharthms607@gmail.com>
2021-03-01 12:45:23 +0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:wastetastic/screens/BasicTestingScreen.dart';
 import 'package:wastetastic/screens/MainScreen.dart';
 import 'package:wastetastic/screens/POI_DetailScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
